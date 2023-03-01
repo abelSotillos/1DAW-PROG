505ffb7d7088e0104c8c4f5349176375652bd006
Abel Sotillos <Abel Sotillos@2DAW-16.2SMR.GSD.local>
2023-03-01 11:04:26 +0100

    ultimo cambio

--- ProveedoresGasoil-asotillos/src/main/java/coop/gsd/daw/prog/App.java
+++ ProveedoresGasoil-asotillos/src/main/java/coop/gsd/daw/prog/App.java
@@ -10,9 +10,7 @@ public class App {
 		}
 		Proveedor proveedor1= new Proveedor(args[0]);
 		Proveedor proveedor2= new Proveedor(args[1]);
-		System.out.println(proveedor1.getNombre());
 		System.out.print(proveedor1.toString());
-		System.out.println(proveedor2.getNombre());
 		System.out.print(proveedor2.toString()); 
 		System.out.println("Importe del 3/1/2023 Proveedor:"+proveedor1.getNombre()+" Importe:"+proveedor1.getImporte(3,1,2023));
 		System.out.println("Media mensual del 1/2023 Proveedor:"+proveedor2.getNombre()+" Media:"+proveedor2.getMediaMensual(1,2023));
