6e2169cf47047d09de7afe55f59949e90962a3f5
Abel Sotillos <Abel Sotillos@2DAW-16.2SMR.GSD.local>
2023-03-22 13:30:21 +0100

    Seguimos con alumnos

--- CsvToDb-asotillos/src/main/java/coop/gsd/daw/prog/Alumno.java
+++ CsvToDb-asotillos/src/main/java/coop/gsd/daw/prog/Alumno.java
@@ -1,5 +1,12 @@
 package coop.gsd.daw.prog;
 
 public class Alumno {
-
+	private String Modulo,id1;
+	private int numLista;
+	public Alumno(String modulo, String id1, int numLista) {
+		Modulo = modulo;
+		this.id1 = id1;
+		this.numLista = numLista;
+	}
+	
 }
--- CsvToDb-asotillos/src/main/java/coop/gsd/daw/prog/Fichero.java
+++ CsvToDb-asotillos/src/main/java/coop/gsd/daw/prog/Fichero.java
@@ -1,7 +1,28 @@
 package coop.gsd.daw.prog;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Fichero {
-	private String Modulo,id1;
-	private int numLista;
+	private Alumno [] Alumnos;
+	public Fichero(String ruta) {
+		try {
+			File f = new File(ruta);
+			Scanner inputFile = new Scanner( f );
+			int i=0;
+			while (inputFile.hasNext()) {
+				String [] line = inputFile.nextLine().split(" ");
+				int num=Integer.parseInt(line[2]);
+				Alumnos[i]=new Alumno(line[0],line[1],num);
+				i++;
+			}
+			inputFile.close();
+		} 
+		catch (FileNotFoundException e) {
+			System.err.println("ERROR: no se puede abrir el fichero");
+		}
+	
+	}
 	
 }
